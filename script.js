1e7da6323c89a79a11f6c9152112e7126cad87f3
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 15:45:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+// function firstWord(s) {
+//   // your code here
+// }
+
+// // Do not change the code below
+
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
+
+
 function firstWord(s) {
-  // your code here
+  // Remove leading and trailing whitespaces
+  const trimmedString = s.trim();
+
+  // Find the index of the first space or the end of the string
+  const spaceIndex = trimmedString.indexOf(' ');
+
+  // Extract the first word based on the space index
+  const firstWord = spaceIndex !== -1 ? trimmedString.slice(0, spaceIndex) : trimmedString;
+
+  return firstWord;
 }
 
-// Do not change the code below
-
+// Example
 const s = prompt("Enter String:");
 alert(firstWord(s));
